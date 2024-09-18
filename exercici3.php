251c7d8e6635ac968c4f51584b22a9238a9ad977
VíctorManuel <manuelluoficial@gmail.com>
2024-09-18 20:22:30 +0200

    commitº2

---
+++ exercici3.php
@@ -0,0 +1,9 @@
+<?php
+//1. Se declaren dos variables con valores numéricos superiores a 10 y seguidamente: 
+$num1 = 73;
+$num2 = 51;
+
+//2. Muestra la progresión numérica de los números pares desde 0 hasta el valor de la primera con un bucle for. 
+for ($i = 0; $i < $num1; $i++) {
+    echo "";
+}
